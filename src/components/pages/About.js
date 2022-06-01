4e0988584345bd36f623ec6ddc01795c79b94ab1
YiLin Ong <chauntelleinc@gmail.com>
2022-05-31 21:23:22 -0400

    added resume to About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,45 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+
 
 export default function About() {
+
   return (
     <div>
-      <h1>About Page</h1>
+
+    <Card className="card-header">ABOUT</Card>    
+    <Card className="card-2">
+
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      <br/ >
+
+      Please take a look at my <a
+      href="https://drive.google.com/file/d/1CjPwpdx-feef7jiRHdqPyMPbmLj6W2Iq/view?usp=sharing"
+      download
+    >
+      Resume
+    </a> 📃.
+    <br/ >    <br/ >    
+
+       <h2>Skills:</h2>
+      <li>React</li>
+      <li>Heroku</li>
+      <li>Github</li>
+      <li>JSON</li>
+      <li>Javascript</li>
+      <li>Java</li>
+      <li>Full-Stack Development</li>
+      <li>CSS</li>
+      <li>Bootstrap/Bulma</li>
+      <li>HTML5</li>
+      <li>MySQL</li>
+      <li>Node.js</li>
+      <li>Express.js</li>
+      <li>ES6</li>
+      <li>Command Line</li>
       </p>
-    </div>
+    </Card>  
+    
+    </div>  
   );
 }
